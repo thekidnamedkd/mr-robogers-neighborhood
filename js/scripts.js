4fcbecef2767cc9ad078e750d82af8a22c50ceca
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-12 14:29:19 -0700

    FINALLY make the range a count from 0 - input number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let roboOut = function(toCount) {
   let range = []
   for (i = 0; i <= toCount; i++)
-    range.push(toCount)
+    range.push(i)
     let countStr = toCount.toString();
     if (countStr.includes("1")) { 
       range.splice(i, 1, "Beep!");
@@ -18,23 +18,8 @@ $(document).ready(function() {
     event.preventDefault();
       const toCount = parseInt($("input#numInput").val());
       const newRobo = roboOut(toCount)
-  //     if (Number.isNaN(numInput)) {
-  //       alert("Please enter a numeric value.")
-  //     }
-
-  //     else if (numInput === 1) {
-  //       alert("beep")
-  //     }
-
-  //     else if (numInput === 2) {
-  //       alert("boop")
-  //     }
-
-  //     else if (numInput === 3) {
-  //       alert("NEIGHBOR")
-  //     }
-  //     else if (Number.isInteger(numInput)) {
-  //       alert("Thank you for the number.")
-  //     }
+      if (Number.isNaN(toCount)) {
+        alert("Please enter a numeric value.")
+      }
   });
 });
