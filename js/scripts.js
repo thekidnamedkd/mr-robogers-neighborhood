e85ce1de7d57d6f72ba33347fdaef7426b1119a6
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-12 10:37:26 -0700

    fix non-numeric inputs to display original alert with numeric prompt. also add a new spec and being testing how to achieve splitting the input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
+const numArray = numInput.split("")
+      console.log(numArray)
+
 $(document).ready(function() {
   $("form#robogers").submit(function(event) {
     event.preventDefault();
       const numInput = parseInt($("input#numInput").val());
-      console.log(numInput);
 
-        if (numInput === numInput) {
-          alert("boop")
-        }
+      if (Number.isNaN(numInput)) {
+        alert("Please enter a numeric value.")
+      }
+
+      else if (Number.isInteger(numInput)) {
+        alert("boop")
+      }
+
+      else if (numArray[0] === 3)
+        alert("beep")
   });
 });
