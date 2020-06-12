b1425a8a89ad414b2085f9e207642a4791ee46ba
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-12 14:00:32 -0700

    add functionality to detect a 1 from the input and add to end of array 'range'. not count the range yet.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 let roboOut = function(toCount) {
   let range = []
   for (i = 0; i <= toCount; i++)
-  range.push(toCount-1)
+    range.push(toCount)
+    let countStr = toCount.toString();
+    if (countStr.includes("1")) { 
+      range.splice(i, 1, "Beep!");
+    }
   console.log(range)
 }
 
@@ -9,7 +13,7 @@ $(document).ready(function() {
   $("form#robogers").submit(function(event) {
     event.preventDefault();
       const toCount = parseInt($("input#numInput").val());
-      const result = roboOut(toCount)
+      const newRobo = roboOut(toCount)
   //     if (Number.isNaN(numInput)) {
   //       alert("Please enter a numeric value.")
   //     }
